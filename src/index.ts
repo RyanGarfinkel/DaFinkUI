cc6c4758308327c6579a3515f54f9916bb0ad44b
RyanGarfinkel <113050972+RyanGarfinkel@users.noreply.github.com>
2026-06-12 09:27:47 -0400

    chore: export new components from src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,14 @@ export type { AccordionType, AccordionProps } from './components/Accordion/Accor
 export { default as Collapsible, CollapsibleTrigger, CollapsibleContent } from './components/Collapsible/Collapsible';
 export type { CollapsibleProps, CollapsibleTriggerProps, CollapsibleContentProps } from './components/Collapsible/Collapsible';
 
+export { CodeBlock } from './components/CodeBlock/CodeBlock';
+export type { CodeBlockVariant, CodeBlockProps } from './components/CodeBlock/CodeBlock';
+
 export { Alert } from './components/Alert/Alert';
 export type { AlertVariant } from './components/Alert/Alert';
+
+export { default as Avatar, AvatarGroup } from './components/Avatar/Avatar';
+export type { AvatarProps, AvatarGroupProps, AvatarSize, AvatarShape } from './components/Avatar/Avatar';
 
 export { Badge } from './components/Badge/Badge';
 export type { BadgeVariant } from './components/Badge/Badge';
@@ -21,6 +27,9 @@ export type { ButtonProps, ButtonVariant, ButtonSize } from './components/Button
 
 export { Canvas } from './components/Canvas/Canvas';
 
+export { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext, CarouselDots } from './components/Carousel/Carousel';
+export type { CarouselProps, CarouselContentProps, CarouselItemProps, CarouselPreviousProps, CarouselNextProps, CarouselDotsProps } from './components/Carousel/Carousel';
+
 export { Card, CardHeader, CardContent, CardFooter } from './components/Card/Card';
 export type { CardVariant } from './components/Card/Card';
 
@@ -31,6 +40,9 @@ export { Checkbox } from './components/Checkbox/Checkbox';
 
 export { Combobox } from './components/Combobox/Combobox';
 export type { ComboboxSize, ComboboxSingleProps, ComboboxMultiProps } from './components/Combobox/Combobox';
+
+export { default as CountUp } from './components/CountUp/CountUp';
+export type { CountUpProps } from './components/CountUp/CountUp';
 
 export { default as CommandPalette, CommandGroup, CommandItem } from './components/CommandPalette/CommandPalette';
 export type { CommandPaletteProps, CommandGroupProps, CommandItemProps } from './components/CommandPalette/CommandPalette';
@@ -44,6 +56,12 @@ export type { PaginatorProps, PaginatorVariant } from './components/DataTable/Pa
 export { DatePicker } from './components/DatePicker/DatePicker';
 export type { DatePickerProps } from './components/DatePicker/DatePicker';
 
+export { default as Drawer, DrawerHeader, DrawerTitle, DrawerContent, DrawerFooter, DrawerClose } from './components/Drawer/Drawer';
+export type { DrawerProps, DrawerSide, DrawerHeaderProps, DrawerTitleProps, DrawerContentProps, DrawerFooterProps, DrawerCloseProps } from './components/Drawer/Drawer';
+
+export { default as DropdownMenu } from './components/DropdownMenu/DropdownMenu';
+export type { DropdownMenuProps, DropdownMenuEntry, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuSide, DropdownMenuAlign } from './components/DropdownMenu/DropdownMenu';
+
 export { FormSection, FormField, FormLabel, FormControl, FormDescription, FormMessage } from './components/Form/Form';
 
 export { default as FunctionPlotter } from './components/FunctionPlotter/FunctionPlotter';
@@ -54,15 +72,30 @@ export type { InputProps } from './components/Input/Input';
 export { KanbanBoard, KanbanCard, KanbanColumn } from './components/Kanban/Kanban';
 export type { KanbanCardProps, KanbanColumnProps } from './components/Kanban/Kanban';
 
+export { default as Modal, ModalHeader, ModalTitle, ModalContent, ModalFooter, ModalClose } from './components/Modal/Modal';
+export type { ModalProps, ModalHeaderProps, ModalTitleProps, ModalContentProps, ModalFooterProps, ModalCloseProps } from './components/Modal/Modal';
+
 export { default as Mosaic, MosaicTile } from './components/Mosaic/Mosaic';
 export type { MosaicTileLayout, MosaicTileSize, MosaicProps, MosaicTileProps } from './components/Mosaic/Mosaic';
+
+export { default as OTPInput } from './components/OTPInput/OTPInput';
+export type { OTPInputProps } from './components/OTPInput/OTPInput';
+
+export { default as Popover } from './components/Popover/Popover';
+export type { PopoverProps, PopoverSide, PopoverAlign } from './components/Popover/Popover';
 
 export { Progress } from './components/Progress/Progress';
 
 export { RadioGroup, RadioItem } from './components/Radio/Radio';
 
+export { default as Reveal, RevealGroup } from './components/Reveal/Reveal';
+export type { RevealProps, RevealGroupProps, RevealEffect } from './components/Reveal/Reveal';
+
 export { Select } from './components/Select/Select';
 export type { SelectOption, SelectSize } from './components/Select/Select';
+
+export { default as Slider } from './components/Slider/Slider';
+export type { SliderProps } from './components/Slider/Slider';
 
 export { Sidebar, SidebarSection, SidebarLink, SidebarDivider } from './components/Sidebar/Sidebar';
 
@@ -82,6 +115,9 @@ export { Tabs, TabsList, TabsTrigger, TabsContent } from './components/Tabs/Tabs
 export { default as Textarea } from './components/Textarea/Textarea';
 export type { TextareaProps } from './components/Textarea/Textarea';
 
+export { default as TextShimmer } from './components/TextShimmer/TextShimmer';
+export type { TextShimmerProps } from './components/TextShimmer/TextShimmer';
+
 export { Timeline, TimelineItem } from './components/Timeline/Timeline';
 export type { TimelineVariant, TimelineDirection, TimelineAnimate } from './components/Timeline/Timeline';
 
@@ -90,6 +126,9 @@ export type { ToastVariant, ToastPosition } from './components/Toast/Toast';
 
 export { default as ToggleGroup, ToggleGroupItem } from './components/ToggleGroup/ToggleGroup';
 export type { ToggleGroupType, ToggleGroupSize } from './components/ToggleGroup/ToggleGroup';
+
+export { default as Tooltip } from './components/Tooltip/Tooltip';
+export type { TooltipProps, TooltipSide } from './components/Tooltip/Tooltip';
 
 export { default as Tree, TreeItem } from './components/Tree/Tree';
 export type { TreeProps } from './components/Tree/Tree';
